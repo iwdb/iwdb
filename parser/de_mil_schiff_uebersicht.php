5c1c8fbff58c7835ab71f4d5b8ce1cbc56d03f47
Mac <MacXY@herr-der-mails.de>
2012-05-11 00:12:48 +0200

    BugFix issue #42 - schiffsuebersicht

--- parser/de_mil_schiff_uebersicht.php
+++ parser/de_mil_schiff_uebersicht.php
@@ -40,83 +40,80 @@ error_reporting(E_ALL);
 
 function parse_de_mil_schiff_uebersicht ( $return )
 {    
-    global $db, $db_tb_schiffstyp, $db_tb_schiffe, $db_tb_user, $selectedusername, $config_date, $deleted;
-      
-    // Variableninitialisierung
-    $deleted = false;
+    global $db, $db_tb_schiffstyp, $db_tb_schiffe, $db_tb_user, $selectedusername, $config_date;
     
 //    foreach ($return->objResultData->aKolos as $kolo)
 //	{
         //! Mac: @todo: Kolonien pruefen ?
 //	}
     
-    foreach ($return->objResultData->aSchiffe as $aschiff)
-	{
-        if (empty($aschiff->strSchiffName)) continue;
-                                       
-        // Name des Schiffstyps bestimmen
-        $schiffsname = $aschiff->strSchiffName;
+    if ($return->bSuccessfullyParsed) {
+        // Setze Zeitpunkt des letzten Schiffsimportes 
+        $sql = "DELETE FROM " . $db_tb_schiffe . 
+            " WHERE user='" . $selectedusername . "'";
+        $result = $db->db_query($sql)
+            or error(GENERAL_ERROR, 
+                'Could not query config information.', '', 
+                __FILE__, __LINE__, $sql);
 
-        // Massdriver Pakete sind keine gültigen Schiffe. 
-        if ( $schiffsname !== "Massdriver Paket" ) {
-            // Suche ID des Schiffstyps in der DB
-            $sql = "SELECT id FROM " . $db_tb_schiffstyp . 
-                " WHERE schiff LIKE '" . $schiffsname . "' OR schiff LIKE '" . $schiffsname . "'";               
-            $result = $db->db_query($sql)
-                or error(GENERAL_ERROR, 
-                    'Could not query config information.', '', 
-                    __FILE__, __LINE__, $sql);
-            $row = $db->db_fetch_array($result); 
-
-            // ID nicht gefunden -> neu einfügen.
-            if (empty($row['id'])) {
-                echo "unbekanntes Schiff gefunden: $schiffsname<br />";
-            /*
-                $schiffsname = str_replace("%%","%", $schiffsname);
-                $schiffsname = str_replace("%"," ", $schiffsname); 
-
-                echo "<div class='doc_red'>Neues Schiff wurde hinzugef&uuml;gt:<br><pre>";
-                print("[".$schiffsname."]");
-                echo "</pre></div> ";   
-                $sql = "INSERT INTO " . $db_tb_schiffstyp . 
-                " (schiff, abk, typ) " .
-                "VALUES" . 
-                " ('" . $schiffsname . "', '" . $schiffsname . "', '')";
-                $result = $db->db_query($sql)
-                    or error(GENERAL_ERROR, 
-                    'Could not query config information.', '', 
+        $sql = "UPDATE " . $db_tb_user . " SET lastshipscan='" . $config_date . 
+            "' WHERE sitterlogin='" . $selectedusername . "'";
+        $result = $db->db_query($sql)
+            or error(GENERAL_ERROR, 
+                'Could not query config information.', '',
                     __FILE__, __LINE__, $sql);
 
-                $row['id'] = mysql_insert_id();
-                */
-            }
-            if (!$deleted) {         
-                // Setze Zeitpunkt des letzten Schiffsimportes 
-                $sql = "DELETE FROM " . $db_tb_schiffe . 
-                    " WHERE user='" . $selectedusername . "'";
+        foreach ($return->objResultData->aSchiffe as $aschiff)
+        {
+            if (empty($aschiff->strSchiffName)) continue;
+
+            // Name des Schiffstyps bestimmen
+            $schiffsname = $aschiff->strSchiffName;
+
+            // Massdriver Pakete sind keine gültigen Schiffe. 
+            if ( $schiffsname !== "Massdriver Paket" ) {
+                // Suche ID des Schiffstyps in der DB
+                $sql = "SELECT id FROM " . $db_tb_schiffstyp . 
+                    " WHERE schiff LIKE '" . $schiffsname . "' OR schiff LIKE '" . $schiffsname . "'";               
                 $result = $db->db_query($sql)
                     or error(GENERAL_ERROR, 
                         'Could not query config information.', '', 
                         __FILE__, __LINE__, $sql);
+                $row = $db->db_fetch_array($result); 
 
-                $sql = "UPDATE " . $db_tb_user . " SET lastshipscan='" . $config_date . 
-                    "' WHERE sitterlogin='" . $selectedusername . "'";
+                // ID nicht gefunden -> neu einfügen.
+                if (empty($row['id'])) {
+                    echo "unbekanntes Schiff gefunden: $schiffsname<br />";
+                /*
+                    $schiffsname = str_replace("%%","%", $schiffsname);
+                    $schiffsname = str_replace("%"," ", $schiffsname); 
+
+                    echo "<div class='doc_red'>Neues Schiff wurde hinzugef&uuml;gt:<br><pre>";
+                    print("[".$schiffsname."]");
+                    echo "</pre></div> ";   
+                    $sql = "INSERT INTO " . $db_tb_schiffstyp . 
+                    " (schiff, abk, typ) " .
+                    "VALUES" . 
+                    " ('" . $schiffsname . "', '" . $schiffsname . "', '')";
+                    $result = $db->db_query($sql)
+                        or error(GENERAL_ERROR, 
+                        'Could not query config information.', '', 
+                        __FILE__, __LINE__, $sql);
+
+                    $row['id'] = mysql_insert_id();
+                    */
+                }
+
+                $sql = "INSERT INTO " . $db_tb_schiffe . 
+                " (user, schiff, anzahl) VALUES ('" . 
+                $selectedusername . "', '" . $row['id'] . "', '" . 
+                $aschiff->iCountGesamt. "')";
                 $result = $db->db_query($sql)
                     or error(GENERAL_ERROR, 
-                        'Could not query config information.', '',
-                            __FILE__, __LINE__, $sql);
-                $deleted = true;
-            }
-
-            $sql = "INSERT INTO " . $db_tb_schiffe . 
-            " (user, schiff, anzahl) VALUES ('" . 
-            $selectedusername . "', '" . $row['id'] . "', '" . 
-            $aschiff->iCountGesamt. "')";
-            $result = $db->db_query($sql)
-                or error(GENERAL_ERROR, 
-                'Could not query config information.', '', 
-                __FILE__, __LINE__, $sql);
-        }   
+                    'Could not query config information.', '', 
+                    __FILE__, __LINE__, $sql);
+            }   
+        }
     }
 }
 

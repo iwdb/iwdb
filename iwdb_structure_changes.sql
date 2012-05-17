7fee1d96b66dcd51b5efb4a7414b4db4eba9a384
balouh <cs.patsch@googlemail.com>
2012-05-17 15:09:59 +0200

    removed collate, added one drop table
    Signed-off-by: balouh <cs.patsch@googlemail.com>

--- iwdb_structure_changes.sql
+++ iwdb_structure_changes.sql
@@ -1,7 +1,10 @@
 -- Tabellenstruktur `prefix_sid` geändert by masel 25.04. 1:00
 ALTER TABLE  `prefix_sid` ENGINE = MEMORY;
 ALTER TABLE  `prefix_sid` CHANGE  `date`  `date` INT( 10 ) UNSIGNED NOT NULL;
-ALTER TABLE  `prefix_sid` CHANGE  `ip`  `ip` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE  `prefix_sid` CHANGE  `ip`  `ip` VARCHAR( 50 ) CHARACTER SET utf8;
 
 -- Tabellenstruktur `prefix_research_user` geändert by patsch 08.05. 21:00
 ALTER TABLE  `prefix_research_user` ADD `time` INT( 13 ) UNSIGNED NOT NULL;
+
+-- Tabelle `iwdb_transport_einstellungen` entfernen, wird nicht benötigt ... by patsch 17.05. 15:00
+DROP TABLE IF EXISTS `iwdb_transport_einstellungen`

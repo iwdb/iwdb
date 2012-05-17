2cbe5b08f5927526e60891c536cc62b095399813
masel <masel789@googlemail.com>
2012-05-17 23:34:47 +0300

    fixing collations to standard utf8

--- iwdb_structure_changes.sql
+++ iwdb_structure_changes.sql
@@ -7,4 +7,14 @@ ALTER TABLE  `prefix_sid` CHANGE  `ip`  `ip` VARCHAR( 50 ) CHARACTER SET utf8;
 ALTER TABLE  `prefix_research_user` ADD `time` INT( 13 ) UNSIGNED NOT NULL;
 
 -- Tabelle `iwdb_transport_einstellungen` entfernen, wird nicht benötigt ... by patsch 17.05. 15:00
-DROP TABLE IF EXISTS `iwdb_transport_einstellungen`
+DROP TABLE IF EXISTS `iwdb_transport_einstellungen`;
+
+-- fixing collations to standard utf8 (utf8_general_ci) by masel 17.05.2012 22:33
+ALTER TABLE  `prefix_gebaeude_spieler` DEFAULT CHARACTER SET utf8;
+ALTER TABLE  `prefix_gebaeude_spieler` CHANGE  `kolo_typ`  `kolo_typ` VARCHAR( 20 ) CHARACTER SET utf8 NOT NULL;
+ALTER TABLE  `prefix_gebaeude_spieler` CHANGE  `user`  `user` VARCHAR( 30 ) CHARACTER SET utf8 NOT NULL;
+ALTER TABLE  `prefix_gebaeude_spieler` CHANGE  `category`  `category` VARCHAR( 100 ) CHARACTER SET utf8 NOT NULL;
+ALTER TABLE  `prefix_gebaeude_spieler` CHANGE  `building`  `building` VARCHAR( 200 ) CHARACTER SET utf8 NOT NULL;
+
+ALTER TABLE  `prefix_user_research` DEFAULT CHARACTER SET utf8;
+ALTER TABLE  `prefix_user_research` CHANGE  `user`  `user` VARCHAR( 30 ) CHARACTER SET utf8;

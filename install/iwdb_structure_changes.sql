f25f89f581c437b920257e589dfc5e2c0ceef699
balouh <cs.patsch@googlemail.com>
2012-05-20 17:23:46 +0200

    fixed bug wrong table name
    Signed-off-by: balouh <cs.patsch@googlemail.com>

--- install/iwdb_structure_changes.sql
+++ install/iwdb_structure_changes.sql
@@ -4,7 +4,7 @@ ALTER TABLE  `prefix_sid` CHANGE  `date`  `date` INT( 10 ) UNSIGNED NOT NULL;
 ALTER TABLE  `prefix_sid` CHANGE  `ip`  `ip` VARCHAR( 50 ) CHARACTER SET utf8;
 
 -- Tabellenstruktur `prefix_research_user` geändert by patsch 08.05. 21:00
-ALTER TABLE  `prefix_research_user` ADD `time` INT( 13 ) UNSIGNED NOT NULL;
+ALTER TABLE  `prefix_user_research` ADD `time` INT( 13 ) UNSIGNED NOT NULL;
 
 -- Tabelle `iwdb_transport_einstellungen` entfernen, wird nicht benötigt ... by patsch 17.05. 15:00
 DROP TABLE IF EXISTS `iwdb_transport_einstellungen`;

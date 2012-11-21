229103ddd88241154b72c3edd3638d3fadb464a2
masel <masel789@googlemail.com>
2012-11-21 14:15:43 +0100

    fix #104

--- modules/login.php
+++ modules/login.php
@@ -25,7 +25,11 @@
 /*****************************************************************************/
 
 //direktes Aufrufen verhindern
-if (basename($_SERVER['PHP_SELF']) != "index.php") {header('HTTP/1.1 404 not found');exit;};
+if (basename($_SERVER['PHP_SELF']) != "index.php") {
+    header('HTTP/1.1 404 not found');
+    exit;
+}
+;
 
 //****************************************************************************
 ?>
@@ -33,21 +37,21 @@ if (basename($_SERVER['PHP_SELF']) != "index.php") {header('HTTP/1.1 404 not fou
 <div class='doc_title'>Login</div>
 
 <?php
-if(( isset($login_id) ) && ($login_ok === FALSE) && ($action != "memberlogout2" )) {
-  doc_message("Falscher Benutzername oder Passwort!");
+if ((isset($login_id)) && ($login_ok === false) && ($action != "memberlogout2")) {
+    echo "<div class='doc_warning'>Falscher Benutzername oder Passwort!</div>\n";
 
-	if( $wronglogins < $config_wronglogins ) {
-    doc_message("Du hast noch " . ($config_wronglogins - $wronglogins) . 
-                " Versuch(e).");
-  }
-    
-	if( $wronglogins >= $config_wronglogins ) { 
-    doc_message("Du hast dich " . $wronglogins . " mal falsch eingeloggt! " .
-                "Einloggen für die nächsten " .
-                round($config_wronglogin_timeout / HOUR) .
-                " Stunden gesperrt.<br>". 
-                "Daten wurden an den Admin übermittelt.");
-  }
+    if ($wronglogins < $config_wronglogins) {
+        echo "<div class='doc_warning'>Du hast noch " . ($config_wronglogins - $wronglogins) . " Versuch(e).</div>\n";
+    }
+
+    if ($wronglogins >= $config_wronglogins) {
+        echo "<div class='doc_warning'>Du hast dich " . $wronglogins . " mal falsch eingeloggt! " .
+            "Einloggen für die nächsten " .
+            round($config_wronglogin_timeout / HOUR) .
+            " Stunden gesperrt.<br>" .
+            "Daten wurden an den Admin übermittelt.</div>\n";
+
+    }
 }
 ?>
 
@@ -71,7 +75,8 @@ if(( isset($login_id) ) && ($login_ok === FALSE) && ($action != "memberlogout2" 
             </td>
         </tr>
         <tr>
-            <td class='titlebg' align='center' colspan='2'><input type='submit' value='lass mich rein' name='B1' class='submit'>
+            <td class='titlebg' align='center' colspan='2'><input type='submit' value='lass mich rein' name='B1'
+                                                                  class='submit'>
             </td>
         </tr>
     </table>

ee2aa0419a8991ade67c43cd0e264a66c1f69c1b
masel <masel789@googlemail.com>
2012-11-23 09:09:41 +0100

    kleine Änderungen der Allykasseparser

--- parser/de_alli_kasse_inhalt.php
+++ parser/de_alli_kasse_inhalt.php
@@ -1,74 +1,62 @@
 <?php
-/*****************************************************************************/
-/* de_alli_kasse_inhalt.php                                                   */
-/*****************************************************************************/
-/* This program is free software; you can redistribute it and/or modify it   */
-/* under the terms of the GNU General Public License as published by the     */
-/* Free Software Foundation; either version 2 of the License, or (at your    */
-/* option) any later version.                                                */
-/*                                                                           */
-/* This program is distributed in the hope that it will be useful, but       */
-/* WITHOUT ANY WARRANTY; without even the implied warranty of                */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General */
-/* Public License for more details.                                          */
-/*                                                                           */
-/* The GNU GPL can be found in LICENSE in this directory                     */
-/*****************************************************************************/
+/**
+ * Parsermodule for the alli bank parser
+ *
+ * This parsemodule is responsible for analysing the alli bank parser data
+ * for help see
+ * @link https://handels-gilde.org/?www/forum/index.php;board=1099.0 the devforum
+ * or
+ * @link https://github.com/iwdb/iwdb the repo at github
+ *
+ * @author    Mac <MacXY@herr-der-mails.de>
+ * @copyright Mac <MacXY@herr-der-mails.de>
+ * @license   GNU GPL version 2 or any later version
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package   iwdb
+ * @subpackage parsermodule
+ */
 
-/*****************************************************************************/
-/* Diese Erweiterung der urspuenglichen DB ist ein Gemeinschafftsprojekt von */
-/* IW-Spielern.                                                              */
-/*                                                                           */
-/* Autor: Mac (MacXY@herr-der-mails.de)                                      */
-/* Datum: Jun 2009 - April 2012                                              */
-/*                                                                           */
-/* Bei Problemen kannst du dich an das eigens dafür eingerichtete            */
-/* Entwicklerforum wenden:                                                   */
-/*        httpd://handels-gilde.org/?www/forum/index.php;board=1099.0        */
-/*                   https://github.com/iwdb/iwdb                            */
-/*                                                                           */
-/*****************************************************************************/
+if (!defined('IRA')) {
+    header('HTTP/1.1 403 forbidden');
+    exit;
+}
 
-if (basename($_SERVER['PHP_SELF']) != "index.php")
-  die('Hacking attempt...!!');
-
-if (!defined('IRA'))
-    die('Hacking attempt...');
-
-error_reporting(E_ALL);
-
-function parse_de_alli_kasse_inhalt ( $return )
+function parse_de_alli_kasse_inhalt($return)
 {
 
-  global $db, $db_tb_scans, $db_tb_kasse_content, CURRENT_UNIX_TIME, $db_tb_user, $user_id, $user_sitterlogin;
+    global $db, $db_tb_kasse_content, $db_tb_user, $user_id;
 
-  $seluser = getVar('seluser') ?  getVar('seluser') : $user_sitterlogin;
+    // ally vom user herausfinden
+    $allianz = "";
 
-  $allianz = "";
-  // ally vom user herausfinden
-  //$sql = "SELECT DISTINCT allianz FROM $db_tb_scans WHERE user like '" . $seluser . "'";
-    $sql = "SELECT allianz FROM " . $db_tb_user . " WHERE id = '" . $user_id . "'";
-  $result = $db->db_query($sql)
-    or error(GENERAL_ERROR, 'Could not query config information.', '', __FILE__, __LINE__, $sql);
-  while($row = $db->db_fetch_array($result)) 
-  {
-    $allianz = $row['allianz'];
-  }
-  if (!empty($return->objResultData->strAlliance)) {
-      $allianz = $return->objResultData->strAlliance;
-  }
-  if (empty($allianz)) {
-      echo "zugehörige Allianz konnte nicht ermittelt werden<br />";
-      return;
-  }
-  
-  $content=$return->objResultData->fCredits;
-    
-  $sql = "REPLACE INTO $db_tb_kasse_content (amount, time_of_insert, allianz) 
-          VALUES ($content, NOW(), '$allianz')";
-  $db->db_query($sql)
-  or error(GENERAL_ERROR, 'Could not query config information.', '', __FILE__, __LINE__, $sql);   
-  
-  //echo "<p><b>Allykasse updated: $content</b></p>\n";
+    //wenn vorhanden aus den parseinformationen holen
+    if (!empty($return->objResultData->strAlliance)) {
+        $allianz = $return->objResultData->strAlliance;
+    }
+
+    //oder aus den IWDB Accinformationen
+    if (empty($allianz)) {
+
+        $sql = "SELECT allianz FROM " . $db_tb_user . " WHERE id = '" . $user_id . "'";
+        $result = $db->db_query($sql)
+            or error(GENERAL_ERROR, 'Could not query config information.', '', __FILE__, __LINE__, $sql);
+        while ($row = $db->db_fetch_array($result)) {
+            $allianz = $row['allianz'];
+        }
+    }
+
+    if (empty($allianz)) {
+        echo "zugehörige Allianz konnte nicht ermittelt werden<br />";
+
+        return;
+    }
+
+    $content = $return->objResultData->fCredits;
+
+    $sql = "REPLACE INTO $db_tb_kasse_content (amount, time_of_insert, allianz)
+          VALUES ($content, " . CURRENT_UNIX_TIME . ", '$allianz')";
+    $db->db_query($sql)
+        or error(GENERAL_ERROR, 'Could not query config information.', '', __FILE__, __LINE__, $sql);
+
+    //echo "<p><b>Allykasse updated: $content</b></p>\n";
 }
-?>
--- parser/de_alli_kasse_log_member.php
+++ parser/de_alli_kasse_log_member.php
@@ -1,72 +1,60 @@
 <?php
-/*****************************************************************************/
-/* de_alli_kasse_log_member.php                                              */
-/*****************************************************************************/
-/* This program is free software; you can redistribute it and/or modify it   */
-/* under the terms of the GNU General Public License as published by the     */
-/* Free Software Foundation; either version 2 of the License, or (at your    */
-/* option) any later version.                                                */
-/*                                                                           */
-/* This program is distributed in the hope that it will be useful, but       */
-/* WITHOUT ANY WARRANTY; without even the implied warranty of                */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General */
-/* Public License for more details.                                          */
-/*                                                                           */
-/* The GNU GPL can be found in LICENSE in this directory                     */
-/*****************************************************************************/
+/**
+ * Parsermodule for the alli bank parser
+ *
+ * This parsemodule is responsible for analysing the alli bank parser data
+ * for help see
+ * @link https://handels-gilde.org/?www/forum/index.php;board=1099.0 the devforum
+ * or
+ * @link https://github.com/iwdb/iwdb the repo at github
+ *
+ * @author    Mac <MacXY@herr-der-mails.de>
+ * @copyright Mac <MacXY@herr-der-mails.de>
+ * @license   GNU GPL version 2 or any later version
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package   iwdb
+ * @subpackage parsermodule
+ */
 
-/*****************************************************************************/
-/* Diese Erweiterung der urspuenglichen DB ist ein Gemeinschafftsprojekt von */
-/* IW-Spielern.                                                              */
-/*                                                                           */
-/* Autor: Mac (MacXY@herr-der-mails.de)                                      */
-/* Datum: Jun 2009 - April 2012                                              */
-/*                                                                           */
-/* Bei Problemen kannst du dich an das eigens dafür eingerichtete            */
-/* Entwicklerforum wenden:                                                   */
-/*        httpd://handels-gilde.org/?www/forum/index.php;board=1099.0        */
-/*                   https://github.com/iwdb/iwdb                            */
-/*                                                                           */
-/*****************************************************************************/
+if (!defined('IRA')) {
+    header('HTTP/1.1 403 forbidden');
+    exit;
+}
 
-
-if (basename($_SERVER['PHP_SELF']) != "index.php")
-  die('Hacking attempt...!!');
-
-if (!defined('IRA'))
-    die('Hacking attempt...');
-
-error_reporting(E_ALL);
-
-function parse_de_alli_kasse_log_member($return) 
+function parse_de_alli_kasse_log_member($return)
 {
-    global $db, $db_tb_scans, $db_tb_kasse_outgoing, CURRENT_UNIX_TIME, $user_sitterlogin, $db_tb_user , $user_id;
+    global $db, $db_tb_kasse_outgoing, $db_tb_user, $user_id;
 
-    $seluser = getVar('seluser') ?  getVar('seluser') : $user_sitterlogin;
-
-    $allianz = "";
     // ally vom user herausfinden
-    //$sql = "SELECT DISTINCT allianz FROM $db_tb_scans WHERE user like '" . $seluser . "'";
-    $sql = "SELECT allianz FROM " . $db_tb_user . " WHERE id = '" . $user_id . "'";
-	$result = $db->db_query($sql)
-        or error(GENERAL_ERROR, 'Could not query config information.', '', __FILE__, __LINE__, $sql);
-    while($row = $db->db_fetch_array($result)) 
-    {
-        $allianz = $row['allianz'];
-    }
+    $allianz = "";
+
+    //wenn vorhanden aus den parseinformationen holen
     if (!empty($return->objResultData->strAlliance)) {
         $allianz = $return->objResultData->strAlliance;
     }
+
+    //oder aus den IWDB Accinformationen
+    if (empty($allianz)) {
+
+        $sql = "SELECT allianz FROM " . $db_tb_user . " WHERE id = '" . $user_id . "'";
+        $result = $db->db_query($sql)
+            or error(GENERAL_ERROR, 'Could not query config information.', '', __FILE__, __LINE__, $sql);
+        while ($row = $db->db_fetch_array($result)) {
+            $allianz = $row['allianz'];
+        }
+    }
+
     if (empty($allianz)) {
         echo "zugehörige Allianz konnte nicht ermittelt werden<br />";
+
         return;
     }
 
 //      //27.05.2007 08:55 von ZAHLENDER an EMPFAENGER 10.000 Credits ausgezahlt
 //		//07.07.2010 20:28 von Labasu an Labasu 20.000 Credits ausgezahlt Grund war kisbau.
-		
+
     foreach ($return->objResultData->aLogs as $log) {
-        $strTime = strftime('%Y-%m-%d %H:%M:00',$log->iDateTime);
+        $strTime = strftime('%Y-%m-%d %H:%M:00', $log->iDateTime);
         $sql = "REPLACE INTO $db_tb_kasse_outgoing (payedfrom, payedto, amount, time_of_pay, allianz) 
                 VALUES ('$log->strFromUser', '$log->strToUser', '$log->iCredits', '$strTime', '$allianz')";
         $db->db_query($sql)
@@ -80,7 +68,5 @@ function parse_de_alli_kasse_log_member($return)
                 . "} Allizahlung [$allianz] von " . $log->strFromUser . " an " . $log->strToUser
                 . " mit " . $log->iCredits . " Credits erkannt." . ( (!empty($log->strReason)) ? (" (" . $log->strReason . ")") : "") ."<br />\n";            
 		*/
-	}
+    }
 }
-
-?>

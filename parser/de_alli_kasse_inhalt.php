f86f0e19cbc74249325290a70e21371366154c00
Mac <MacXY@herr-der-mails.de>
2012-12-02 13:42:04 +0100

    BugFix #107: Eintragung des Datums als String

--- parser/de_alli_kasse_inhalt.php
+++ parser/de_alli_kasse_inhalt.php
@@ -52,7 +52,7 @@ function parse_de_alli_kasse_inhalt($return)
     $content = $return->objResultData->fCredits;
 
     $sql = "REPLACE INTO $db_tb_kasse_content (amount, time_of_insert, allianz)
-          VALUES ($content, ".strftime('%Y-%m-%d %H:%M:00', CURRENT_UNIX_TIME).", '$allianz')";
+          VALUES ($content, '".strftime('%Y-%m-%d %H:%M:00', CURRENT_UNIX_TIME)."', '$allianz')";
 
     $db->db_query($sql)
         or error(GENERAL_ERROR, 'Could not query config information.', '', __FILE__, __LINE__, $sql);

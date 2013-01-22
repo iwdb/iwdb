086d0ca098097caed992b434992866f04344f34a
masel <masel789@googlemail.com>
2013-01-22 20:10:42 +0100

    #129

--- parser/de_xml.php
+++ parser/de_xml.php
@@ -345,8 +345,9 @@ function save_sbxml($scan_data)
     }
     // Nebel vorhanden
     if (isset($scan_data['nebula'])) {
-        $db->db_update($db_tb_sysscans, array('nebula' => $scan_data['nebula']), "WHERE `gal`=" . $scan_data['coords_gal'] . "AND `sys`=" . $scan_data['coords_sys'])
-            or error(GENERAL_ERROR, 'Could not query config information.', '', __FILE__, __LINE__, $sql);
+        $sql = "INSERT INTO `{$db_tb_sysscans}` SET `nebula`='".$scan_data['nebula']."' WHERE `gal`=".$scan_data['coords_gal'] . " AND `sys`=" . $scan_data['coords_sys']." ON DUPLICATE KEY UPDATE `nebula`='".$scan_data['nebula'] . "';";
+        $db->db_query($sql)
+            or error(GENERAL_ERROR, 'Could not update nebula information.', '', __FILE__, __LINE__, $sql);
         unset($scan_data['nebula']);
     }
     // INSERT

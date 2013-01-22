e9b91619f8d514d51e64237482bd5a8c5c6335f3
masel <masel789@googlemail.com>
2013-01-22 17:22:19 +0100

    #127

--- javascript/sc.js
+++ javascript/sc.js
@@ -6,25 +6,29 @@ function SondenRechnen() {
     "use strict";
     var X11Benoetigt, TerminusBenoetigt, X13Benoetigt;
 
-    //limitieren auf 4 Stellen
+    //Limitieren des Input auf max 4 Stellen
     document.getElementById('SD01').value = document.getElementById('SD01').value.slice(0, 4);
     document.getElementById('SD02').value = document.getElementById('SD02').value.slice(0, 4);
 
-    //kein leeres Eingabefeld
-    if (document.getElementById('SD01').value === '') {
+    var AnzahlSD01 = +document.getElementById('SD01').value;
+    var AnzahlSD02 = +document.getElementById('SD02').value;
+
+    //Input validieren
+    if (isNaN(AnzahlSD01) || (AnzahlSD01 < 0)) {
+        AnzahlSD01 = 0;
         document.getElementById('SD01').value = 0;
     }
-    if (document.getElementById('SD02').value === '') {
+    if (isNaN(AnzahlSD02) || (AnzahlSD02 < 0)) {
+        AnzahlSD02 = 0;
         document.getElementById('SD02').value = 0;
     }
 
-    var AnzahlSD01 = +document.getElementById('SD01').value || 0;
-    var AnzahlSD02 = +document.getElementById('SD02').value || 0;
-
+    //Anzahl Sonden berechnen
     X11Benoetigt = Math.ceil(AnzahlSD01 + (AnzahlSD02 * 2.5) + 20);
     TerminusBenoetigt = Math.ceil((AnzahlSD01 / 1.2) + (AnzahlSD02 * 2.5 / 1.2) + 10);
     X13Benoetigt = Math.ceil((AnzahlSD01 / 2) + (AnzahlSD02 * 2.5 / 2) + 8);
 
+    //Ausgabe
     document.getElementById('X11').firstChild.data = X11Benoetigt.toString();
     document.getElementById('Terminus').firstChild.data = TerminusBenoetigt.toString();
     document.getElementById('X13').firstChild.data = X13Benoetigt.toString();

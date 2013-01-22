fd33483574753274ed42666f373138da04b8e03f
masel <masel789@googlemail.com>
2013-01-22 16:33:18 +0100

    #127

--- javascript/sc.js
+++ javascript/sc.js
@@ -6,11 +6,20 @@ function SondenRechnen() {
     "use strict";
     var X11Benoetigt, TerminusBenoetigt, X13Benoetigt;
 
+    //limitieren auf 4 Stellen
     document.getElementById('SD01').value = document.getElementById('SD01').value.slice(0, 4);
     document.getElementById('SD02').value = document.getElementById('SD02').value.slice(0, 4);
 
-    var AnzahlSD01 = parseInt(document.getElementById('SD01').value, 10) || 0;
-    var AnzahlSD02 = parseInt(document.getElementById('SD02').value, 10) || 0;
+    //kein leeres Eingabefeld
+    if (document.getElementById('SD01').value === '') {
+        document.getElementById('SD01').value = 0;
+    }
+    if (document.getElementById('SD02').value === '') {
+        document.getElementById('SD02').value = 0;
+    }
+
+    var AnzahlSD01 = +document.getElementById('SD01').value || 0;
+    var AnzahlSD02 = +document.getElementById('SD02').value || 0;
 
     X11Benoetigt = Math.ceil(AnzahlSD01 + (AnzahlSD02 * 2.5) + 20);
     TerminusBenoetigt = Math.ceil((AnzahlSD01 / 1.2) + (AnzahlSD02 * 2.5 / 1.2) + 10);

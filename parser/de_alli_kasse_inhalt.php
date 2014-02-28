e3f81bca7a89a8e95b9f48f52a406fa97be781f0
masel <masel789@gmail.com>
2014-02-28 17:20:22 +0100

    fix Allykasseparser

--- parser/de_alli_kasse_inhalt.php
+++ parser/de_alli_kasse_inhalt.php
@@ -32,10 +32,10 @@ function parse_de_alli_kasse_inhalt($return)
 
     $SQLdata = array();
     $SQLdata['amount'] = $return->objResultData->fCredits;
-    $SQLdata['time_of_insert'] = strftime('%Y-%m-%d %H:%M:%S', CURRENT_UNIX_TIME);
+    $SQLdata['time_of_insert'] = strftime('%Y-%m-%d %H:%M:00', CURRENT_UNIX_TIME);
     $SQLdata['allianz'] = $user_allianz;
 
-    $db->db_update($db_tb_kasse_content, $SQLdata);
+    $db->db_insertupdate($db_tb_kasse_content, $SQLdata);
 
     echo "<div class='doc_message'>Inhalt (" . number_format($SQLdata['amount'], 2, ",", ".") . " Credits) aktualisiert</div>";
 }
